58a0549656b1c16c8f51961b9d679f8cec0b616e
sergey <50322776+sergeytalyzin@users.noreply.github.com>
2019-09-08 15:19:06 +0300

    popup

--- source/js/script.js
+++ source/js/script.js
@@ -14,3 +14,33 @@ navToggle.addEventListener('click', function() {
     header.classList.remove('header--js');
   }
 });
+
+
+var form = document.querySelector(".form__button");
+var formtrue = document.querySelector(".modal__true");
+var formerror = document.querySelector(".modal__error");
+var name = document.querySelector(".form__name");
+var surname = document.querySelector(".form__surname");
+var email = document.querySelector(".form__email");
+var closeerror = document.querySelector(".modal__true-button");
+var closetrue = document.querySelector("modal__error-button");
+
+
+
+form.addEventListener("click", function(evt) {
+  if (!name.value || !surname.value || !email.value) {
+    evt.preventDefault();
+    formerror.classList.add("modal-show")}
+    else {
+    formtrue.classList.add("modal-show")
+  }});
+closeerror.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  formerror.classList.remove("modal-show");
+});
+closetrue.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  formtrue.classList.remove("modal-show");
+});
+
+
